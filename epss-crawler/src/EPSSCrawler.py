67de998dfeda086a19fead5dbbb602d4a28256fd
dravalico <damianoravalico.uni@gmail.com>
2023-11-28 15:30:48 +0100

    Created base things for EPSSCrawler

---
+++ epss-crawler/src/EPSSCrawler.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+class EPSSCrawler:
+    def __int__(self):
+        pass
+
+    def run(self):
+        pass
---
+++ epss-crawler/src/main.py
@@ -0,0 +1,4 @@
+from EPSSCrawler import EPSSCrawler
+
+if __name__ == '__main__':
+    EPSSCrawler().run()
